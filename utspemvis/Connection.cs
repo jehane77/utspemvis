51c04d8bc7e5ed786b1a6342f46aeba6405ed8a8
Socrates Sullivan Riadi Putra <socrates.sullivan@gmail.com>
2018-07-27 18:00:13 +0700

    Adding Usefull Comment

--- utspemvis/Connection.cs
+++ utspemvis/Connection.cs
@@ -11,12 +11,16 @@ namespace utspemvis
 {
     public class Connection
     {
+        //random function
         static Random rand = new Random();
+
+        //get random into int
         public static int a = rand.Next(2, 51);
         
-        
+        //connection to database, DO NOT MESS WITH THIS
         public static OleDbConnection con = new OleDbConnection("Provider=Microsoft.Jet.OLEDB.4.0;Data Source=|DataDirectory|/Database.mdb");
         
+        //querry for database, DO NOT MESS IF U DONT UNDERSTAND
         public static OleDbCommand cmd = new OleDbCommand("SELECT Jawaban, Resource FROM Table1 WHERE ID = '" + a + "'", con);
 
     }
--- utspemvis/Endless.cs
+++ utspemvis/Endless.cs
@@ -20,32 +20,45 @@ namespace utspemvis
         string jawab = "";
         string resSoal;
         int score, life = 5;
+
+        //function for calling (something) from Form2
         Form2 form2 = new Form2();
 
+        //initialize 
         void init()
         {
+            //get path to image
             string fullPath;
             string path1 = @"../../Resources/";
             fullPath = Path.GetFullPath(path1);
 
+            //open connection
             Connection.con.Open();
+
+            //read from database
             OleDbDataReader dr = Connection.cmd.ExecuteReader();
             dr.Read();
 
+            //insert data from database to var
             soal = dr.GetString(0);
             resSoal = dr.GetString(1);
+
+            //close read and connection
             dr.Close();
             Connection.cmd.Connection = Connection.con;
             Connection.cmd.ExecuteNonQuery();
             Connection.con.Close();
 
+            //load pict from (path)
             pictureBox1.Load(fullPath + resSoal);
             label1.Text = Convert.ToString(life);
+
             textBox1.Text = "";
         }
 
         public Endless()
         {
+            //happend before Form load
             InitializeComponent();
             init();
         }
@@ -55,14 +68,13 @@ namespace utspemvis
             
         }
 
-
-
-
         private void button1_Click(object sender, EventArgs e)
         {
-            
+            //lowercase text from textbox
             jawab = textBox1.Text.ToLower();
             label1.Text = jawab;
+
+            //game logic
             if (jawab == soal)
             {
                 MessageBox.Show("Jawaban Benar");
@@ -71,14 +83,21 @@ namespace utspemvis
             }
             else
             {
+                //false condition
                 life = life-1;
+
+                //has life
                 if(life > 0)
                 {
                     MessageBox.Show("Jawaban Salah, Coba Lagi");
                 }
+
+                //has no life
                 else
                 {
                     MessageBox.Show("Anda Kalah");
+
+                    //closing form endless
                     this.Close();
                     form2.Visible = true;
                 }
